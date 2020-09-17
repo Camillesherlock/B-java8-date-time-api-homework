1ae1a35329c5a84fcacd6c769612db2f4b8d85d0
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-17 23:15:45 +0800

    [lk] finish practice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,6 +1,11 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * 对任意日期获取下一个工作日, 不考虑节假日
@@ -11,8 +16,23 @@ import java.time.LocalDate;
 public class Practice2 {
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    
-
-    return null;
+      LocalDate nextWorkday = date;
+      ZoneId zone = ZoneId.systemDefault();
+      Instant instant = date.atStartOfDay().atZone(zone).toInstant();
+      java.util.Date upDate = Date.from(instant);
+      Calendar cal = Calendar.getInstance();
+      cal.setTime(upDate);
+      int w = cal.get(Calendar.DAY_OF_WEEK)-1;
+      if(w==5)
+      {
+          nextWorkday = date.plusDays(3);
+      }else
+      if(w==6){
+          nextWorkday = date.plusDays(2);
+      }
+      else{
+          nextWorkday = date.plusDays(1);
+      }
+    return nextWorkday;
   }
 }
